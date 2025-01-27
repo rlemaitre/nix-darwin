d0aa2bf9290b786e6485ed5abc98d06060f06863
Raphaël Lemaitre <raphael@rlemaitre.com>
2025-01-27 01:16:22 +0100

    feat: add pistol

--- home/default.nix
+++ home/default.nix
@@ -11,6 +11,7 @@
 #    ./gw.nix
     ./gpg.nix
     ./bat.nix
+    ./pistol.nix
   ];
 
   # home manager needs this
@@ -22,17 +23,23 @@
 
   # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/darwin/
   home.packages = with pkgs; [
+    chafa
     coreutils
     eza # modern replacement for `ls`
     delta
     fd # modern replacement for `find`
     fzf
+    fzf-git-sh
+    gh
+    gh-f
     git
     gnupg
+    hexyl
     lazygit
     neovim
     oh-my-posh
     pinentry_mac
+    pistol
     ripgrep
     tldr
     tree-sitter
