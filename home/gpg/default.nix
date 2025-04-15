8ead98931fa57b846bf53b5614169c3ee240da21
Raphaël Lemaitre <raphael@rlemaitre.com>
2025-04-15 23:16:26 +0200

    fix: fix GPG config

--- home/gpg/default.nix
+++ home/gpg/default.nix
@@ -3,13 +3,17 @@
   programs.gpg = {
     enable = true;
     package = pkgs.gnupg24;
+    homedir= "${userConfig.homeDirectory}/.local/share/gnupg";
+    scdaemonSettings = {
+      "reader-port" = "Yubico Yubi";
+    };
   };
 
   home.packages = with pkgs; [ pinentry_mac ];
 
   # gnu --batch --import
   #  https://github.com/NixOS/nixpkgs/issues/240819#issuecomment-1616760598
-  home.file.".gnupg/gpg-agent.conf".text = ''
+  home.file.".local/share/gnupg/gpg-agent.conf".text = ''
     # seconds after the last GnuPG activity
     default-cache-ttl 28800
     default-cache-ttl-ssh 28800
@@ -22,7 +26,7 @@
     allow-preset-passphrase
 
     # mac
-    pinentry-program "${userConfig.homeDirectory}/.gnupg/pinentry-ide.sh"
+    pinentry-program "${userConfig.homeDirectory}/.local/share/gnupg/pinentry-ide.sh"
 
     enable-ssh-support
 
@@ -33,4 +37,19 @@
     # you don't have one
     # disable-scdaemon
   '';
+  home.file.".local/share/gnupg/pinentry-ide.sh" = {
+    executable = true;
+    text = ''
+      #!/bin/sh
+      if [ -n "$PINENTRY_USER_DATA" ]; then
+        case "$PINENTRY_USER_DATA" in
+          IJ_PINENTRY=*)
+            "/Applications/IntelliJ IDEA.app/Contents/jbr/Contents/Home/bin/java" -cp "/Applications/IntelliJ IDEA.app/Contents/plugins/vcs-git/lib/git4idea-rt.jar:/Applications/IntelliJ IDEA.app/Contents/lib/externalProcess-rt.jar" git4idea.gpg.PinentryApp
+            exit $?
+          ;;
+        esac
+      fi
+      exec /opt/homebrew/bin/pinentry-mac "$@"
+    '';
+  };
 }
